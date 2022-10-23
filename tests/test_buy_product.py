561ad74a90d9110701ce92831d73b7dc0eb83133
mikhailrezchikov <mrezchikov529@gmail.com>
2022-10-23 22:36:36 +0400

    cart page added

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from pages.cart_page import Cart_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
 
@@ -18,5 +19,7 @@ def test_buy_product():
     login.autorization()
     mp = Main_page(driver)
     mp.select_product()
+    cp = Cart_page(driver)
+    cp.product_confirmation()
 
     time.sleep(5)
